bddd57b7756d0d3c78d902ec842ad32b0c273376
Faeka Ansari <faeka6@gmail.com>
2024-11-27 22:18:50 +0530

    automate formula generation
    Signed-off-by: Faeka Ansari <faeka6@gmail.com>

--- README.md
+++ README.md
@@ -18,8 +18,37 @@ brew untap akuity/tap
 
 ## How to Update the Tap
 
-TODO
+To update the tap for a new Kargo release
+
+Clone the tap repository:
+```bash
+git clone https://github.com/akuity/homebrew-tap.git
+cd homebrew-tap
+```
+
+Use the `update.sh` script to update the formula:
+```bash
+./update.sh kargo <VERSION>
+```
+Replace `<VERSION>` with the new Kargo version. For example: `./update.sh kargo v1.0.0`
+
+Verify the updated formula:
+```bash
+brew install --build-from-source ./kargo.rb
+```
+
+Test the installation to ensure the correct version is installed:
+```bash
+kargo --version
+```
+
+Commit and push the changes:
+```bash
+git add kargo.rb
+git commit -m "Update Kargo formula to version <VERSION>"
+git push origin main
+```
 
 ## Documentation
 
-`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh/)
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh/) for further guidance.
---
+++ update.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+set -e -x
+
+if [ "$#" -lt 2 ]; then
+  echo "Usage: ./update.sh <kargo> VERSION"
+  exit 1
+fi
+
+CLI_NAME="$1"
+VERSION="$2"
+BREW_VERSION="$3"
+
+if [ "${CLI_NAME}" = "kargo" ]; then
+  URL_BASE="https://github.com/akuity/kargo/releases/download"
+  CLASSNAME="Kargo"
+  DESC="Application lifecycle orchestration"
+else
+  echo "Unsupported binary: ${CLI_NAME}"
+  exit 1
+fi
+
+# OSX - Intel
+OSX_INTEL_CLI_NAME="${CLI_NAME}-darwin-amd64"
+OSX_INTEL_BINPATH="/tmp/${OSX_INTEL_CLI_NAME}"
+curl --fail -L -o ${OSX_INTEL_BINPATH} -s "${URL_BASE}/${VERSION}/${OSX_INTEL_CLI_NAME}" || (echo "Failed to curl (${URL_BASE}/${VERSION}/${OSX_INTEL_CLI_NAME})" && exit 1)
+OSX_INTEL_SHA256=$(shasum -a 256 "${OSX_INTEL_BINPATH}" | awk '{print $1}')
+
+# OSX - ARM
+OSX_ARM_CLI_NAME="${CLI_NAME}-darwin-arm64"
+OSX_ARM_BINPATH="/tmp/${OSX_ARM_CLI_NAME}"
+curl --fail -L -o ${OSX_ARM_BINPATH} -s "${URL_BASE}/${VERSION}/${OSX_ARM_CLI_NAME}" || (echo "Failed to curl (${URL_BASE}/${VERSION}/${OSX_ARM_CLI_NAME})" && exit 1)
+OSX_ARM_SHA256=$(shasum -a 256 "${OSX_ARM_BINPATH}" | awk '{print $1}')
+
+# Linux - Intel
+LINUX_INTEL_CLI_NAME="${CLI_NAME}-linux-amd64"
+LINUX_INTEL_BINPATH="/tmp/${LINUX_INTEL_CLI_NAME}"
+curl --fail -L -o ${LINUX_INTEL_BINPATH} -s "${URL_BASE}/${VERSION}/${LINUX_INTEL_CLI_NAME}" || (echo "Failed to curl (${URL_BASE}/${VERSION}/${LINUX_INTEL_CLI_NAME})" && exit 1)
+LINUX_INTEL_SHA256=$(shasum -a 256 "${LINUX_INTEL_BINPATH}" | awk '{print $1}')
+
+# Linux - ARM
+LINUX_ARM_CLI_NAME="${CLI_NAME}-linux-arm64"
+LINUX_ARM_BINPATH="/tmp/${LINUX_ARM_CLI_NAME}"
+curl --fail -L -o ${LINUX_ARM_BINPATH} -s "${URL_BASE}/${VERSION}/${LINUX_ARM_CLI_NAME}" || (echo "Failed to curl (${URL_BASE}/${VERSION}/${LINUX_ARM_CLI_NAME})" && exit 1)
+LINUX_ARM_SHA256=$(shasum -a 256 "${LINUX_ARM_BINPATH}" | awk '{print $1}')
+
+CLASS_POSTFIX=$(echo ${BREW_VERSION} | tr -d '.')
+CLASS_POSTFIX=$(echo ${CLASS_POSTFIX} | sed "s/@/AT/g")
+TEMPLATE="# This is an auto-generated file. DO NOT EDIT
+class ${CLASSNAME}${CLASS_POSTFIX} < Formula
+    desc \"${DESC}\"
+    homepage \"https://kargo.akuity.io/\"
+    version \"${VERSION}\"
+
+    on_macos do
+      if Hardware::CPU.intel?
+        url \"${URL_BASE}/${VERSION}/${OSX_INTEL_CLI_NAME}\"
+        sha256 \"${OSX_INTEL_SHA256}\"
+      elsif Hardware::CPU.arm?
+        url \"${URL_BASE}/${VERSION}/${OSX_ARM_CLI_NAME}\"
+        sha256 \"${OSX_ARM_SHA256}\"
+      end
+    end
+
+    on_linux do
+      if Hardware::CPU.intel?
+        url \"${URL_BASE}/${VERSION}/${LINUX_INTEL_CLI_NAME}\"
+        sha256 \"${LINUX_INTEL_SHA256}\"
+      elsif Hardware::CPU.arm?
+        url \"${URL_BASE}/${VERSION}/${LINUX_ARM_CLI_NAME}\"
+        sha256 \"${LINUX_ARM_SHA256}\"
+      end
+    end
+
+    def install
+      bin.install \"kargo-#{OS.kernel_name.downcase}-#{Hardware::CPU.arch}\" => \"kargo\"
+    end
+
+    test do
+      assert_match version.to_s, shell_output(\"#{bin}/kargo --version\")
+    end
+end"
+
+echo "${TEMPLATE}" > "${CLI_NAME}${BREW_VERSION}.rb"
